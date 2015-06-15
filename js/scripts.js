19cc3f6a11eb1f01fe917c628097ee385680a041
piestrzynski <dominik@piestrzynski.info>
2015-06-15 16:46:04 +0200

    h1
    dodano naglowek h1

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+/*-------------------
+     MOJE SKRYPTY  
+--------------------*/
+
+$(document).ready(function() {
+//START WEBSITE  READY
+
+	$('body').hide().fadeIn(5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // END 
